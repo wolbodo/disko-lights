0b2b3f05a80d4b06ea695ba50fd0cc3348111fee
Dexter <dexter@dexterlabs.nl>
2022-02-08 20:50:37 +0100

    Setup wifi

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include "twinkle.h"
 #include "display.h"
 
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "Wolbodo";
+const char* password = "darknetwork";
+
 #define ROTARY_1 12
 #define ROTARY_2 14
 
@@ -16,8 +24,62 @@ CRGBPalette16 gTargetPalette;
 
 void setup()
 {
-  delay( 1000 ); //safety startup delay
-	Serial.begin(9600);
+
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   display.setup();
 
 	ESP32Encoder::useInternalWeakPullResistors=UP;
@@ -48,9 +110,10 @@ void runTwinkles() {
 
 }
 
-
 void loop()
 {
+    ArduinoOTA.handle();
+    
   int encoderCount = encoder.getCount() / 2;
   display.show(encoderCount % 100);
   // display.show(millis() / encoderCount % 100);
