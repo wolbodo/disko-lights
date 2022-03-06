c227192c1cda12b7396da2bbe36990582f42a728
Dexter <dexter@dxlb.nl>
2022-03-06 16:24:43 +0100

    something with wifi

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,17 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-
 const char* ssid = "Wolbodo";
 const char* password = "darknetwork";
 
-#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  1        /* Time ESP32 will go to sleep (in seconds) */
-
-#define TILE_WIDTH 4
+#define PANEL_WIDTH 4
+#define PANEL_HEIGHT 4
+#define TILE_WIDTH 6
 #define TILE_HEIGHT 4
-#define GRID_WIDTH 6
-#define GRID_HEIGHT 4
-#define NUM_LEDS GRID_HEIGHT * TILE_HEIGHT * GRID_WIDTH * TILE_WIDTH
+#define GRID_WIDTH PANEL_WIDTH * TILE_WIDTH
+#define GRID_HEIGHT PANEL_HEIGHT * TILE_HEIGHT
+#define TILE_HEIGHT 4
+#define NUM_LEDS TILE_HEIGHT * PANEL_HEIGHT * TILE_WIDTH * PANEL_WIDTH
 
 #define LED_TYPE   WS2813
 #define COLOR_ORDER   GRB
@@ -40,9 +39,12 @@ const char* password = "darknetwork";
 
 #define POWER_BUTTON_PIN GPIO_NUM_4
 
+#define PROGRAM_COUNT 4
+
 
 Display display = Display(SEGMENT_PINS);
 ESP32Encoder encoder;
+int programId = 0;
 
 
 class Matrix {
@@ -58,66 +60,105 @@ class Matrix {
       this->height = height;
     }
 
-    void draw() {
-      int time = millis();
-      for (int i = 0; i < NUM_LEDS; i++) {
-        bool on = (i + (time / 60)) % 100 < 50;
-        leds[i].setHSV((time / 10 + i/10) % 255, on ? 255 : sin8(time/100+i)/2, scale8(sin8(-time / 50 + i/5), 200) + 55);
+    void drawPixel(int x, int y, CRGB color) {
+      // locate tile
+      int PANEL_y = y / PANEL_HEIGHT;
+      int PANEL_x = (PANEL_y % 2)
+        ? x / PANEL_WIDTH
+        : TILE_WIDTH - PANEL_x - 1;
+
+      int tile;
+      int i;
+
+      tile = PANEL_y * TILE_WIDTH + PANEL_x;
+      i = tile * PANEL_HEIGHT * PANEL_WIDTH;
+
+      int _x = x % PANEL_WIDTH;
+      int _y = y % PANEL_HEIGHT;
+      if (_y % 2) {
+        i += _y*PANEL_WIDTH + PANEL_WIDTH - _x - 1;
       }
-    };
-
-    void drawHorizontalLine(int y, CRGB color) {
-      for (int x = 0; x < GRID_WIDTH; x++) {
-        // tile
-        int tile_y = y / TILE_HEIGHT;
-        int tile_x = x / TILE_WIDTH;
-
-        int tile;
-        int i;
-
-        if (tile_y % 2) { // odd
-          tile_x = tile_x;
-        } else { // even
-          tile_x = GRID_WIDTH - tile_x - 1;
-        }
-
-        tile = tile_y * GRID_WIDTH + tile_x;
-        i = tile * TILE_HEIGHT * TILE_WIDTH;
-
-        int _x = x % TILE_WIDTH;
-        int _y = y % TILE_HEIGHT;
-        if (_y % 2) {
-          i += _y*TILE_WIDTH + TILE_WIDTH - _x - 1;
-        }
-        else
-        {
-          i += _y*TILE_WIDTH + _x;
-        }
-
-        this->leds[i] = color;
+      else
+      {
+        i += _y*PANEL_WIDTH + _x;
       }
+
+      this->leds[i] = color;
     };
 };
 
-Matrix matrix = Matrix(GRID_WIDTH, GRID_HEIGHT);
+Matrix matrix = Matrix(TILE_WIDTH, TILE_HEIGHT);
+
+
+void drawRainbows1(Matrix matrix) {
+  int time = millis();
+  for (int i = 0; i < NUM_LEDS; i++) {
+    bool on = (i + (time / 60)) % 100 < 50;
+    matrix.leds[i].setHSV(
+      (time / 10 + i/10) % 255,
+      on ? 255 : sin8(time/100+i)/2,
+      scale8(sin8(-time / 50 + i/5), 200) + 55
+    );
+  }
+};
+
+void drawRainbows2(Matrix matrix) {
+  int time = millis();
+  for (int y = 0; y < TILE_HEIGHT * PANEL_HEIGHT; y++) {
+    for (int x=0; x < TILE_WIDTH * PANEL_WIDTH; x++) {
+      matrix.drawPixel(x, y, CHSV(x * 2 + time/20, 255, 255));
+    }
+  }
+}
 
 void setBrightness() {
   int pot_value = ((float) analogRead(POT_INPUT_PIN)) / 16.062745098039215;
   FastLED.setBrightness(pot_value);
 }
 
-void setup()
-{
+void wifiScan() {
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+}
+
+void setup() {
 
   Serial.begin(115200);
   Serial.println("Booting");
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin(ssid, password);
-  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-  //   Serial.println("Connection Failed! Rebooting...");
-  //   delay(5000);
-  //   ESP.restart();
-  // }
+
+  WiFi.mode(WIFI_STA);
+
+  wifiScan();
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println(WiFi.status());
+    Serial.println("Connection Failed! Retrying...");
+    delay(1000);
+    // ESP.restart();
+  }
 
   // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
@@ -132,37 +173,37 @@ void setup()
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  // ArduinoOTA
-  //   .onStart([]() {
-  //     String type;
-  //     if (ArduinoOTA.getCommand() == U_FLASH)
-  //       type = "sketch";
-  //     else // U_SPIFFS
-  //       type = "filesystem";
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
 
-  //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-  //     Serial.println("Start updating " + type);
-  //   })
-  //   .onEnd([]() {
-  //     Serial.println("\nEnd");
-  //   })
-  //   .onProgress([](unsigned int progress, unsigned int total) {
-  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  //   })
-  //   .onError([](ota_error_t error) {
-  //     Serial.printf("Error[%u]: ", error);
-  //     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-  //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-  //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  //   });
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
 
-  // ArduinoOTA.begin();
+  ArduinoOTA.begin();
 
-  // Serial.println("Ready");
-  // Serial.print("IP address: ");
-  // Serial.println(WiFi.localIP());
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   display.setup();
 
@@ -172,12 +213,9 @@ void setup()
   pinMode(POT_LED_PIN, OUTPUT);
 
   pinMode(ROTARY_PIN_BUTTON, INPUT_PULLDOWN);
-  // pinMode(ROTARY_PIN_A, INPUT_PULLUP);
-  // pinMode(ROTARY_PIN_B, INPUT_PULLUP);
 
 	ESP32Encoder::useInternalWeakPullResistors=UP;
   encoder.attachFullQuad(ROTARY_PIN_A, ROTARY_PIN_B);
-  encoder.setCount(40);
 
   FastLED.setMaxPowerInVoltsAndMilliamps( VOLTS, MAX_MA);
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(matrix.leds, NUM_LEDS)
@@ -186,6 +224,46 @@ void setup()
 
 
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_4, 1);
+}
+
+byte count = 0;
+void runProgram() {
+  int time = millis();
+  switch (programId) {
+    case 0:
+      drawRainbows2(matrix); break;
+    case 1:
+      count ++;
+      for (int i = 0; i < NUM_LEDS; i++) {
+        matrix.leds[i] = CHSV(count, 255, ((count/8) % 2) * 255);
+      }
+      break;
+
+    case 2:
+      count ++;
+      for (int i = 0; i < NUM_LEDS; i++) {
+        matrix.leds[i] = CHSV(count, 255, ((count/8) % 2) * 255);
+      }
+      break;
+    case 3:
+      count ++;
+      for (int i = 0; i < NUM_LEDS; i++) {
+        byte row = i / (GRID_WIDTH * PANEL_HEIGHT) + time / 50 % 2;
+        matrix.leds[i] = CHSV(count, row, ((count/8) % 2) * 255);
+      }
+      break;
+
+    default:
+      drawRainbows1(matrix);
+      break;
+      
+
+    // matrix.drawHorizontalLine((millis() / 1000)+1 % TILE_HEIGHT, CRGB::Black);
+
+    // matrix.drawHorizontalLine(7, CRGB::Black);
+  }
+  // matrix.drawHorizontalLine((millis() / 1000) % TILE_HEIGHT, CRGB::Black);
+  FastLED.show();
 }
 
 
@@ -200,19 +278,18 @@ void loop() {
     esp_deep_sleep_start();
   }
 
-  // ArduinoOTA.handle();
+  ArduinoOTA.handle();
 
-  int encoderCount = encoder.getCount() / 2;
+  programId = (encoder.getCount() / 2) % PROGRAM_COUNT;
   int encoderButton = digitalRead(ROTARY_PIN_BUTTON);
 
   if (millis() / 100 % 2) {
     setBrightness();
   }
 
-  display.showNumber(encoderCount % 100);
+  display.showNumber(programId);
 
   digitalWrite(POT_LED_PIN, encoderButton);
-  // display.show(millis() / encoderCount % 100);
 
   // if (encoderCount != _encoderCount) {
   //   Serial.println("Encoder = " + String((int32_t)encoderCount));
@@ -224,13 +301,6 @@ void loop() {
     //   pixel.setHSV((time / 10) % 255, sin8(time / 33), sin8(time / 70));
     // }
 
-  matrix.draw();
 
-  // matrix.drawHorizontalLine((millis() / 1000) % GRID_HEIGHT, CRGB::Black);
-  // matrix.drawHorizontalLine((millis() / 1000)+1 % GRID_HEIGHT, CRGB::Black);
-
-  // matrix.drawHorizontalLine(7, CRGB::Black);
-
-
-  FastLED.show();
+  runProgram();
 }
