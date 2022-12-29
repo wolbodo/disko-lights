e366fc31bf7f1b47d2edc4f8c224139c8a90304d
itsme <itsme@xs4all.nl>
2022-12-29 03:08:32 +0100

    note that we want to add config to programs

--- lib/Programs/program.h
+++ lib/Programs/program.h
@@ -7,4 +7,7 @@ class Program {
 public:
     virtual void setup() {};
     virtual void tick(Ledgrid&) = 0;
+
+    //virtual void setConfig(JsonObject data) { }
+    //virtual void getConfig(JsonObject data) { }
 };
